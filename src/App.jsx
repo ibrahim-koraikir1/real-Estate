c7de8ee3e0444df5619b7637b343c7eef50d4a3a
ibraim <ikoraikir@gmail.com>
2022-10-22 04:13:55 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="text-red-800">
+     
+    </div>
+  )
+}
+
+export default App
