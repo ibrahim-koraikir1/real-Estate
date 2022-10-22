d8751ce51eec5d7c522819ab1a448a210bb8b9b7
ibraim <ikoraikir@gmail.com>
2022-10-22 07:49:30 -0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  
+import { Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
+// import pages
+import Home from './pages/Home';
+import PropertyDetails from './pages/PropertyDetails';
+
+const App = () => {
   return (
-    <div className="text-red-800">
-     
+    <div className='max-w-[1440px] mx-auto bg-white'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/property/:id' element={<PropertyDetails />} />
+      </Routes>
+      <Footer />
     </div>
-  )
-}
+  );
+};
+
+
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// import link
+import { Link } from 'react-router-dom';
+// import logo
+import Logo from '../assets/react.svg';
+
+const Header = () => {
+  return (
+    <header className='py-6 mb-12 border-b'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link to='/'>
+          <img src={Logo} alt='' />
+        </Link>
+        <div className='flex items-center gap-6'>
+          <Link className='hover:text-violet-900 transition font-bold text-lg' to='/'>
+            buy
+          </Link>
+          <Link className='hover:text-violet-900 transition font-bold text-lg' to='/'>
+            rent
+          </Link>
+         
+        </div>
+        <div className='flex items-center gap-6'>
+          <Link className='hover:text-violet-900 transition' to='/'>
+            Log in
+          </Link>
+          <Link
+            className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition'
+            to='/'
+          >
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
